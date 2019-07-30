dc253c71c185f39caa901ef677bca1431de4e6f7
eliataylor <eli@taylormadetraffic.com>
2019-07-29 22:53:53 -0700

    mostly copied and pasted stuff from a tutorial and Track Authority Music

---
+++ src/Config.js
@@ -0,0 +1,10 @@
+const Config = {
+    api: {
+        base: 'https://localhost:3000',
+        timeout: 0, // Milliseconds
+        base64d:'c2FudGFDbGllbnQ6c2FudGFTZWNyZXQ=',
+        tokName : 'santaToken'
+    }
+};
+
+export default Config;
--- src/redux/authReducer.js
+++
@@ -1,5 +0,0 @@
-// oauth
-
-// my location
-
-// notifications
