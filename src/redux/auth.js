b227aa544f64803e159dd25e294026a0eee956c5
eliataylor <eli@taylormadetraffic.com>
2019-08-02 22:37:26 -0700

    rebuilt auth reducer and actions

--- src/redux/auth.js
+++ src/redux/auth.js
@@ -1,129 +1,86 @@
-export const LOG_IN = 'LOG_IN'
-export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS'
-export const LOG_IN_FAILURE = 'LOG_IN_FAILURE'
-export const LOG_OUT = 'LOG_OUT'
+// events
+export const LOGIN_STARTED = 'LOGIN_STARTED' // for modals / navigation
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS' //
+export const LOGIN_FAILURE = 'LOGIN_FAILURE'
 
-export const SIGN_UP = 'SIGN_UP'
-export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS'
-export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE'
+export const SIGNUP_STARTED = 'SIGNUP_STARTED'
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS' // an opportunity for the client to route what's next
+export const SIGNUP_FAILURE = 'SIGNUP_FAILURE'
 
-export const SHOW_SIGN_IN_CONFIRMATION_MODAL = 'SHOW_SIGN_IN_CONFIRMATION_MODAL'
-export const SHOW_SIGN_UP_CONFIRMATION_MODAL = 'SHOW_SIGN_UP_CONFIRMATION_MODAL'
+export const VERIFY_STARTED = 'VERIFY_STARTED'
+export const VERIFY_SUCCESS = 'VERIFY_SUCCESS' // an opportunity for the client to route what's next
+export const VERIFY_FAILURE = 'VERIFY_FAILURE'
 
-export const CONFIRM_SIGNUP = 'CONFIRM_SIGNUP'
-export const CONFIRM_SIGNUP_SUCCESS = 'CONFIRM_SIGNUP_SUCCESS'
-export const CONFIRM_SIGNUP_FAILURE = 'CONFIRM_SIGNUP_FAILURE'
+export const NEXT_STEP_STARTED = 'NEXT_STEP_STARTED'
+export const NEXT_STEP_SUCCESS = 'NEXT_STEP_SUCCESS' // an opportunity for the server to tell the client what to do next
+export const NEXT_STEP_FAILURE = 'NEXT_STEP_FAILURE'
 
-export const CONFIRM_LOGIN = 'CONFIRM_LOGIN'
-export const CONFIRM_LOGIN_SUCCESS = 'CONFIRM_LOGIN_SUCCESS'
-export const CONFIRM_LOGIN_FAILURE = 'CONFIRM_LOGIN_FAILURE'
+export const LOG_OUT = 'LOG_OUT' // generally async anyway (client kills token, doesn't need callback for revoking on server)
 
+// default authentication object
 const initialState = {
-  isAuthenticating: false,
-  user: {},
+  me: false,
+  token:false,
 
   signUpError: false,
-  signInError: false,
+  verifyError: false,
+  logInError: false,
 
-  showSignUpConfirmationModal: false,
-  showSignInConfirmationModal: false,
-
-  confirmSignUpError: false,
-  confirmLoginError: false,
-
-  signInErrorMessage: '',
-  signUpErrorMessage: '',
-
-  confirmLoginErrorMessage: '',
-  confirmSignUpErrorMessage: ''
+  nextSteps: []
 }
 
 export default (state = initialState, action) => {
+  var newState = {...state};
   switch(action.type) {
-    case SHOW_SIGN_IN_CONFIRMATION_MODAL:
-      return {
-        ...state,
-        isAuthenticating: false,
-        showSignInConfirmationModal: true
-      }
-      case SHOW_SIGN_UP_CONFIRMATION_MODAL:
-      return {
-        ...state,
-        isAuthenticating: false,
-        showSignUpConfirmationModal: true
-      }
-    case CONFIRM_SIGNUP:
-      return {
-        ...state,
-        isAuthenticating: true
-      }
-    case CONFIRM_SIGNUP_SUCCESS:
-      return {
-        ...state,
-        isAuthenticating: false,
-        showSignUpConfirmationModal: false
-      }
-    case CONFIRM_SIGNUP_FAILURE:
-      return {
-        ...state,
-        isAuthenticating: false,
-        confirmSignUpError: false,
-        confirmSignupErrorMessage: action.error.message
-      }
-    case SIGN_UP:
-      return {
-        ...state,
-        isAuthenticating: true,
-      }
-    case SIGN_UP_SUCCESS:
-      return {
-        ...state,
-        isAuthenticating: false
-      }
-    case SIGN_UP_FAILURE:
-      return {
-        ...state,
-        isAuthenticating: false,
-        signUpError: true,
-        signUpErrorMessage: action.error.message
-      }
-    case LOG_IN:
-      return {
-        ...state,
-        isAuthenticating: true,
-        signInError: false
-      }
-    case LOG_IN_SUCCESS:
-      return {
-        isAuthenticating: false,
-        user: action.user,
-        showSignInConfirmationModal: true
-      }
-    case LOG_IN_FAILURE:
-      return {
-        ...state,
-        isAuthenticating: false,
-        signInError: true,
-        signInErrorMessage: action.error.message
-      }
-    case CONFIRM_LOGIN: {
-      return {
-        ...state,
-        isAuthenticating: true
-      }
-    }
-    case CONFIRM_LOGIN_SUCCESS:
-      return {
-        ...state,
-        isAuthenticating: false,
-        showSignInConfirmationModal: false
-      }
-    case CONFIRM_LOGIN_FAILURE: {
-      return {
-        ...state,
-        isAuthenticating: false
-      }
-    }
+
+    case LOGIN_STARTED:
+      newState.logInError = false;
+      return newState;
+    case LOGIN_SUCCESS:
+      newState.logInError = false;
+      newState.me = action.me;
+      newState.token = action.token;
+      return newState;
+    case LOGIN_FAILURE:
+      newState.logInError = action.error;
+      return newState;
+
+    case SIGNUP_STARTED:
+      newState.signUpError = false;
+      return newState;
+    case SIGNUP_SUCCESS:
+      newState.signUpError = false;
+      newState.me = action.me;
+      newState.token = action.token;
+      return newState;
+    case SIGNUP_FAILURE:
+      newState.signUpError = action.error;
+      return newState;
+
+    case VERIFY_STARTED:
+      newState.verifyError = false;
+      return newState;
+    case VERIFY_SUCCESS:
+      newState.verifyError = false;
+      newState.me = action.me;
+      newState.token = action.token;
+      return newState;
+    case VERIFY_FAILURE:
+      newState.verifyError = action.error;
+      return newState;
+
+    case NEXT_STEP_STARTED:
+      newState.logInError = false;
+      return newState;
+    case NEXT_STEP_SUCCESS:
+      newState.logInError = false;
+      newState.me = action.me;
+      newState.token = action.token;
+      return newState;
+    case NEXT_STEP_FAILURE:
+      newState.logInError = action.error;
+      return newState;
+
     case LOG_OUT:
       return {
         ...initialState,
