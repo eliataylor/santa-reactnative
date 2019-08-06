e32581c671d4a5d4a6dd2676c93387eab55caaa6
eliataylor <eli@taylormadetraffic.com>
2019-08-06 12:13:10 -0700

    android building; ios still has issues

---
+++ README.md
@@ -0,0 +1,1 @@
+keytool -genkeypair -v -keystore santa4homeless.keystore -alias santa4homeless -keyalg RSA -keysize 2048 -validity 10000
--- src/theme.js
+++ src/theme.js
@@ -3,12 +3,14 @@ const colors = {
   secondary: '#b9b9b9'
 }
 
+/*
 const fonts = {
   hairline: 'Lato-Hairline',
   light: 'Lato-Light',
   base: 'Lato-Regular',
   bold: 'Lato-Bold'
 }
+*/
 
 export {
   colors
